66b6405d566892622d32c8a2b6bbe864c958d2b0
Maurício Moreira de Aguilar <mmaguylar@hotmail.com>
2016-03-05 10:17:58 -0300

    UzBico Project - Criação de Entidades Básicas

---
+++ uzbico/uzbico01/src/main/java/com/hackathon/uzbico/entities/Competencia.java
@@ -0,0 +1,34 @@
+package com.hackathon.uzbico.entities;
+
+import java.util.List;
+
+public class Competencia {
+
+	private Long id;
+	private String titulo;
+	private List<Usuario> usuarios;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public List<Usuario> getUsuarios() {
+		return usuarios;
+	}
+
+	public void setUsuarios(List<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+}
---
+++ uzbico/uzbico01/src/main/java/com/hackathon/uzbico/entities/Usuario.java
@@ -0,0 +1,53 @@
+package com.hackathon.uzbico.entities;
+
+import java.util.List;
+
+public class Usuario {
+
+	private String cpf;
+	private String nome;
+	private String senha;
+	private String telefone;
+	private List<Competencia> competencias;
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public List<Competencia> getCompetencias() {
+		return competencias;
+	}
+
+	public void setCompetencias(List<Competencia> competencias) {
+		this.competencias = competencias;
+	}
+
+}
